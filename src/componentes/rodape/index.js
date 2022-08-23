bdc33d74657de974f97d36fdc6677f62df893fa7
Uashington <uashington.sm@gmail.com>
2022-08-23 20:44:01 -0300

    Página Home terminada sem estilização

---
+++ src/componentes/rodape/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Rodape = () => (
+    <footer>
+        <div>
+            <img src="/assets/logo.png" alt="" />
+            <div>
+                <h3>ENDEREÇO</h3>
+                <p>Av. Bernardino de Campos, 98</p>
+                <p>São Paulo, SP 12345-678</p>
+            </div>
+            <div>
+                <h3>CONTATO</h3>
+                <p>info@meusite.com</p>
+                <p>Tel: (11) 3456-7890</p>
+            </div>
+            <div>
+                <h3>HORÁRIOS</h3>
+                <p>ABERTO TODOS OS DIAS</p>
+                <p>10:00 - 22:00</p>
+            </div>
+        </div>
+
+        <div>
+            <p>Gelateria. Orgulhosamente desenvolvido por Uashington da Silva Matos</p>
+        </div>
+    </footer>
+);
+
+export default Rodape;
--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Topo from "../../componentes/topo";
+import Rodape from "../../componentes/rodape";
 
 const Home = () => (
     <div>
@@ -22,7 +23,30 @@ const Home = () => (
                 </div>
             </section>
 
+            <section className="secaoEventos">
+                <div>
+                    <h2>Nossos Eventos</h2>
+                    <span>Delícias com sorvete!</span>
+
+                    <p>Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.</p>
+                </div>
+
+                <img src="assets/eventos-image.jpg" alt="" />
+            </section>
+
+            <section className="secaoSobre">
+                <img src="assets/sobre-image.jpg" alt="" />
+
+                <div>
+                    <h2>Sobre Nós</h2>
+                    <span>Alegria em cada casquinha!</span>
+
+                    <p>Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melhor para o 	nosso cliente e você não pode ficar fora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.</p>
+                </div>
+            </section>
+
         </main>
+        <Rodape />
     </div>
 );
 
