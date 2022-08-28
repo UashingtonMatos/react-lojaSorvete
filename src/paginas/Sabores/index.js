71d4f3b2fab94e00731e5e97f38f265528e6e8f4
Uashington <uashington.sm@gmail.com>
2022-08-28 19:50:17 -0300

    Finalizado página sabores

--- src/paginas/Sabores/index.js
+++ src/paginas/Sabores/index.js
@@ -9,49 +9,51 @@ const Sabores = () => (
         <Topo />
 
         <main>
-            <section className="secao-sabores">
-                <div className="container-sabores">
+            <section className="secao-sorvete-banner">
+                <div className="container-sorvete-banner">
                     <h1>NOSSOS SABORES</h1>
                 </div>
             </section>
 
-            <section>
+            <section className="secao-sabores">
                 <h2>SABORES DE SORVETE</h2>
 
-                <div>
-                    <img src="assets/sabor-oreo.png" alt="" />
-                    <h3>Sorvete de Oreo</h3>
-                    <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
-                </div>
+                <div className="container-sabores-card limitar-secao">
+                    <div className="container-sabores-cards">
+                        <img src="assets/sabor-oreo.png" alt="" />
+                        <h3>Sorvete de Oreo</h3>
+                        <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
+                    </div>
 
-                <div>
-                    <img src="assets/sabor-pistache.png" alt="" />
-                    <h3>Sorvete Pistache</h3>
-                    <p>Cremoso sorvete sabor pistache com pedacinhos de semente.</p>
-                </div>
+                    <div className="container-sabores-cards">
+                        <img src="assets/sabor-pistache.png" alt="" />
+                        <h3>Sorvete Pistache</h3>
+                        <p>Cremoso sorvete sabor pistache com pedacinhos de semente.</p>
+                    </div>
 
-                <div>
-                    <img src="assets/sabor-cookies-avela.png" alt="" />
-                    <h3>Sorvete Cookies & Avelã</h3>
-                    <p>O nosso melhor sorvete. Você vai adorar o sabor.</p>
-                </div>
+                    <div className="container-sabores-cards">
+                        <img src="assets/sabor-cookies-avela.png" alt="" />
+                        <h3>Sorvete Cookies & Avelã</h3>
+                        <p>O nosso melhor sorvete. Você vai adorar o sabor.</p>
+                    </div>
 
-                <div>
-                    <img src="assets/sorbet-kiwi.png" alt="" />
-                    <h3>Sorvete de Kiwi</h3>
-                    <p>Delicioso e refrescante sorvete sabor kiwi. Rico em vitamina C.</p>
-                </div>
+                    <div className="container-sabores-cards">
+                        <img src="assets/sorbet-kiwi.png" alt="" />
+                        <h3>Sorvete de Kiwi</h3>
+                        <p>Delicioso e refrescante sorvete sabor kiwi. Rico em vitamina C.</p>
+                    </div>
 
-                <div>
-                    <img src="assets/sorbet-morango.png" alt="" />
-                    <h3>Sorvete de Morango</h3>
-                    <p>Sorvete de morango gourmet. Tradicional e saboroso.</p>
-                </div>
+                    <div className="container-sabores-cards">
+                        <img src="assets/sorbet-morango.png" alt="" />
+                        <h3>Sorvete de Morango</h3>
+                        <p>Sorvete de morango gourmet. Tradicional e saboroso.</p>
+                    </div>
 
-                <div>
-                    <img src="assets/sorbet-limao.png" alt="" />
-                    <h3>Sorvete de Limão Siciliano</h3>
-                    <p>O incrivel sorvete gourmet de limão siciliano vai te encantar.</p>
+                    <div className="container-sabores-cards">
+                        <img src="assets/sorbet-limao.png" alt="" />
+                        <h3>Sorvete de Limão Siciliano</h3>
+                        <p>O incrivel sorvete gourmet de limão siciliano vai te encantar.</p>
+                    </div>
                 </div>
             </section>
 
