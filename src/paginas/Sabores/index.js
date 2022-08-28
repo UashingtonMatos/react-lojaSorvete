c93ebc99d96a4ea124e39ee1bd08b8123d75bd9d
Uashington <uashington.sm@gmail.com>
2022-08-28 17:33:04 -0300

    Página sabores estrutura pronta

--- src/paginas/Sabores/index.js
+++ src/paginas/Sabores/index.js
@@ -1,7 +1,66 @@
 import React from "react";
+import Topo from "../../componentes/topo";
+import Rodape from "../../componentes/rodape";
+
+import './style.css';
 
 const Sabores = () => (
-    <div>Sabores</div>
+    <div>
+        <Topo />
+
+        <main>
+            <section className="secao-sabores">
+                <div className="container-sabores">
+                    <h1>NOSSOS SABORES</h1>
+                </div>
+            </section>
+
+            <section>
+                <h2>SABORES DE SORVETE</h2>
+
+                <div>
+                    <img src="assets/sabor-oreo.png" alt="" />
+                    <h3>Sorvete de Oreo</h3>
+                    <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
+                </div>
+
+                <div>
+                    <img src="assets/sabor-pistache.png" alt="" />
+                    <h3>Sorvete Pistache</h3>
+                    <p>Cremoso sorvete sabor pistache com pedacinhos de semente.</p>
+                </div>
+
+                <div>
+                    <img src="assets/sabor-cookies-avela.png" alt="" />
+                    <h3>Sorvete Cookies & Avelã</h3>
+                    <p>O nosso melhor sorvete. Você vai adorar o sabor.</p>
+                </div>
+
+                <div>
+                    <img src="assets/sorbet-kiwi.png" alt="" />
+                    <h3>Sorvete de Kiwi</h3>
+                    <p>Delicioso e refrescante sorvete sabor kiwi. Rico em vitamina C.</p>
+                </div>
+
+                <div>
+                    <img src="assets/sorbet-morango.png" alt="" />
+                    <h3>Sorvete de Morango</h3>
+                    <p>Sorvete de morango gourmet. Tradicional e saboroso.</p>
+                </div>
+
+                <div>
+                    <img src="assets/sorbet-limao.png" alt="" />
+                    <h3>Sorvete de Limão Siciliano</h3>
+                    <p>O incrivel sorvete gourmet de limão siciliano vai te encantar.</p>
+                </div>
+            </section>
+
+        </main>
+
+        <Rodape />
+    </div>
+
+
 );
 
 export default Sabores;
